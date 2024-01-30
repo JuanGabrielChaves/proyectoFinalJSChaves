dc6365d728f0f88b20dabba65e2791d1018b2855
Juan Gabriel Chaves <achavesjg@gmail.com>
2024-01-29 22:48:40 -0300

    agregado funciones Monedas

--- main.js
+++ main.js
@@ -1,7 +1,87 @@
+//Botones:
 const btnPrestamo = document.querySelector(".solicitar");
+const btnBuscar = document.querySelector("#buscar");
+const btnAllMonedas = document.querySelector(".monedasAll");
+
+//Constantes:
 const MAYOR = 21;
 const MONTO_MINIMO = 10000;
 const TEA = 50;
+
+class Persona {
+  constructor(nombre, apellido, edad, dni, telefono) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.dni = dni;
+    this.telefono = telefono;
+  }
+  esMayor = () => this.edad >= MAYOR;
+  getNombre = () => this.nombre;
+  getApellido = () => this.apellido;
+  getEdad = () => this.edad;
+  getDni = () => this.dni;
+  getTelefono = () => this.telefono;
+}
+
+class SolicitudPrestamo {
+  constructor(Persona, montoSolicitado, cantCuotas) {
+    this.Persona = Persona;
+    this.montoSolicitado = montoSolicitado;
+    this.cantCuotas = cantCuotas;
+  }
+}
+
+let monedas = [
+  { nombre: "Dolar BNA", valor: 804.5 },
+  { nombre: "Dolar MEP", valor: 1218.25 },
+  { nombre: "Dolar Blue", valor: 1249.8 },
+  { nombre: "Euro", valor: 887.97 },
+  { nombre: "Reales", valor: 167.8 },
+  { nombre: "Libra Esterlina", valor: 1041.97 },
+  { nombre: "Yen", valor: 5.6094 },
+  { nombre: "Peso Uruguayo", valor: 20.77 },
+];
+
+function buscarMoneda() {
+  const nombreMonedaInput = document
+    .querySelector("#inputMoneda")
+    .value.trim()
+    .toLowerCase();
+  const monedasCoincidentes = monedas.filter((moneda) =>
+    moneda.nombre.toLowerCase().includes(nombreMonedaInput)
+  );
+  const resultadoDiv = document.querySelector("#resultado");
+  if (monedasCoincidentes.length > 0) {
+    resultadoDiv.innerHTML = "";
+    monedasCoincidentes.forEach((moneda) => {
+      resultadoDiv.innerHTML += `
+              <p>1 <strong>${moneda.nombre}</strong>  =  <strong>$ </strong>${moneda.valor}</p>
+          `;
+    });
+  } else {
+    resultadoDiv.innerHTML = `
+    <p><strong>No se encontró la moneda, por favor vuelva a intentar</strong></p>
+`;
+  }
+}
+
+let valorParaMostrar = monedas.map((moneda) => {
+  return {
+    nombre: moneda.nombre,
+    valor: `1 ${moneda.nombre} = <strong>Pesos Argentinos</strong> $${moneda.valor} `,
+  };
+});
+
+crearGrillaMonedas = () => {
+  const gridContainer = document.querySelector(".grid-container");
+  valorParaMostrar.forEach((moneda) => {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.innerHTML = `<strong>${moneda.nombre}:</strong> ${moneda.valor}`;
+    gridContainer.appendChild(gridItem);
+  });
+};
 
 const esMayor = (edad) => edad >= MAYOR;
 
@@ -38,14 +118,11 @@ const armarForm = (monto, cuotas) => {
   let saldoRestante;
   for (let i = 1; i <= saldoCuotas; i++) {
     saldoTotal -= montoPorCuota;
-    const listItem = document.createElement("li");
-    const mensaje = document.createTextNode(
-      `Cuota ${i} de ${Number(
-        saldoCuotas
-      )}  | Monto de la cuota $ ${montoPorCuota}   |   Saldo faltante $ ${saldoTotal}`
-    );
-    listItem.appendChild(mensaje);
-    document.getElementById("listado").appendChild(listItem);
+    const gridItem = document.createElement("div");
+    gridItem.innerHTML = `Cuota ${i} de ${Number(
+      saldoCuotas
+    )}  | Monto de la cuota $ ${montoPorCuota}   |   Saldo faltante $ ${saldoTotal}`;
+    document.querySelector("#listado").appendChild(gridItem);
   }
 };
 
@@ -63,3 +140,6 @@ const solicitar = () => {
     }
   }
 };
+btnBuscar.addEventListener("click", buscarMoneda);
+btnPrestamo.addEventListener("click", solicitar);
+btnAllMonedas.addEventListener("click", crearGrillaMonedas);
